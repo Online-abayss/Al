cabc9ffec39de971c53960b09e92520b10e628d8
강희인 <dwoosung1@gmail.com>
2023-05-10 22:50:28 +0900

    알고리즘 문제풀이

---
+++ src/backjoon/basic_math/_10026.java
@@ -0,0 +1,25 @@
+package backjoon.basic_math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10026 {
+
+    static boolean[] visited;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+
+
+        }
+
+
+    }
+}
