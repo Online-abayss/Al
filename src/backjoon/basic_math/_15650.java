a36655d0f4335bf0af7b6f2da275c4b5d9cde81c
강희인 <dwoosung1@gmail.com>
2023-06-03 22:12:13 +0900

    알고리즘 문제풀이

---
+++ src/backjoon/basic_math/_15650.java
@@ -0,0 +1,19 @@
+package backjoon.basic_math;
+
+public class _15650 {
+
+    static boolean[] visited;
+
+    static int[] arr;
+
+    static int n;
+    static int m;
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
