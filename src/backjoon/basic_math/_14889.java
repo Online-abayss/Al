ab55f53062137e78760d4dba92f9a86f3082d6c4
강희인 <dwoosung1@gmail.com>
2023-06-07 22:53:39 +0900

    알고리즘 문제풀이

--- src/backjoon/basic_math/_14889.java
+++ src/backjoon/basic_math/_14889.java
@@ -1,4 +1,39 @@
 package backjoon.basic_math;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class _14889 {
+
+    static int t;
+
+    static int[][] arr;
+
+    static
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+
+        arr = new int[t][t];
+
+        for (int i = 0; i < t; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < t; j++) {
+
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < )
+
+
+    }
 }
