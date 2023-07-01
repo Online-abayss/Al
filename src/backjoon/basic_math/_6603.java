bb91df139723d1dc5a563ae50796814f4e45c4f1
강희인 <dwoosung1@gmail.com>
2023-07-01 23:41:22 +0900

    알고리즘 문제풀이

---
+++ src/backjoon/basic_math/_6603.java
@@ -0,0 +1,12 @@
+package backjoon.basic_math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _6603 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
