d9b80ca3e9cc38736f596c03a29067bc2be4ba57
강희인 <dwoosung1@gmail.com>
2023-07-06 23:14:23 +0900

    알고리즘 문제풀이

--- src/backjoon/basic_math/_14225.java
+++ src/backjoon/basic_math/_14225.java
@@ -7,81 +7,85 @@ import java.util.*;
 
 public class _14225 {
 
-    static int t;
-    static int[] map;
-    static int idx;
-    static boolean[] visited;
-    static Set<Integer> sum_lst = new HashSet<>();
+    static int n;
+    static int[] arr;
+    static boolean[] visited = new boolean[20*100000 + 10];
+    static int answer = 1;
 
     public static void main(String[] args) throws IOException {
 
+        // 이해함
+        Scanner sc = new Scanner(System.in);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        t = Integer.parseInt(br.readLine());
-
-        map = new int[t];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < t; i++) {
-
-            map[i] = Integer.parseInt(st.nextToken());
+        n = sc.nextInt();
+        arr = new int[n];
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+        dfs(0,0);
+        while(visited[answer]){
+            answer++;
         }
-
-        Arrays.sort(map);
-
-        visited = new boolean[100001];
-
-
-
-        for (int i = 0; i < t; i ++) {
-
-            int sum = 0;
-            sum_lst.add(map[i]);
-            if (!visited[map[i]]) {
-
-                idx = i;
-                dfs(map[i], 1, sum);
-                visited[map[i]] = false;
-            }
-        }
-
-        System.out.println(sum_lst);
-
-        for (int i = 1; i < 100001; i++) {
-
-            if (!sum_lst.contains(i)) {
-
-                System.out.println(i);
-                break;
-            }
-        }
-
+        System.out.println(answer);
     }
 
-    private static void dfs(int now_number, int depth, int sum) {
-
-        visited[now_number] = true;
-
-        if (depth == t) {
-
-            return;
+    static void dfs(int idx,int sum){
+        if(idx == n){
+            visited[sum] = true;
         }
-
-        for (int i = idx + 1; i < t; i++) {
-
-            if (!visited[map[i]]) {
-
-                sum = now_number + map[i];
-                sum_lst.add(sum);
-                dfs(map[i], depth + 1, sum);
-                visited[map[i]] = false;
-            }
+        else{
+            dfs(idx+1,sum+arr[idx]);
+            dfs(idx+1,sum);
         }
-
-
-
-
-
     }
+
 }
+
+// 망겜.
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        t = Integer.parseInt(br.readLine());
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        map = new int[t];
+//
+//        for (int i = 0; i < t; i++) {
+//
+//            map[i] = Integer.parseInt(st.nextToken());
+//
+//        }
+//
+//        visited = new boolean[100001];
+//
+//        for (int i = 0; i < t; i++) {
+//
+//            if (!visited[map[i]]) {
+//
+//                sum_lst.add(map[i]);
+//                dfs(map[i], 0);
+//                visited[map[i]] = false;
+//            }
+//        }
+//
+//        System.out.println(sum_lst);
+//    }
+//
+//    private static void dfs(int sum_number, int depth) {
+//
+//        visited[sum_number] = true;
+//
+//        if (depth == 3) {
+//
+//            return;
+//        }
+//
+//        for (int i = 0; i < t; i ++) {
+//
+//             if (!visited[map[i]]) {
+//
+//                 sum_number = sum_number + map[i];
+//                 sum_lst.add(sum_number);
+//                 dfs(sum_number, depth + 1);
+//                 visited[map[i]] = false;
+//             }
+//        }
+//    }
