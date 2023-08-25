602e713cafe3651c8358196215eceb68a944e3bc
강희인 <dwoosung1@gmail.com>
2023-08-25 19:53:31 +0900

    알고리즘 문제풀이

---
+++ src/Programmers/level_2/기능개발.java
@@ -0,0 +1,28 @@
+package Programmers.level_2;
+
+import java.util.Arrays;
+
+public class 기능개발 {
+
+    public static void main(String[] args) {
+
+
+        int[] progresses = {93, 30 , 55};
+        int[] speeds = {1, 30 , 5};
+
+        Solution solution = new Solution();
+
+        System.out.println(Arrays.toString(solution.solution(progresses, speeds)));
+
+    }
+
+    static class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            int[] answer = {};
+
+
+
+            return answer;
+        }
+    }
+}
--- src/Programmers/level_2/의상.java
+++ src/Programmers/level_2/의상.java
@@ -3,6 +3,7 @@ package Programmers.level_2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class 의상 {
 
@@ -23,6 +24,26 @@ public class 의상 {
         public int solution(String[][] clothes) {
             int answer = 0;
 
+            Map<String, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < clothes.length; i++) {
+
+                if (map.get(clothes[i][1]) == null) {
+
+                    map.put(clothes[i][1], 1);
+                }
+                else {
+
+                    map.put(clothes[i][1], map.get(clothes[i][1]) +1);
+                }
+            }
+
+            for (String s : map.keySet()) {
+
+                answer += (map.get(s) + 1);
+            }
+
+            answer --;
 
             return answer;
         }
