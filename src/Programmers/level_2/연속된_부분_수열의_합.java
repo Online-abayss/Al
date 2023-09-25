141d6823852b79d71f6ab96782241592a22f7f82
강희인 <dwoosung1@gmail.com>
2023-09-25 23:58:32 +0900

    알고리즘 문제풀이

--- src/Programmers/level_2/연속된_부분_수열의_합.java
+++ src/Programmers/level_2/연속된_부분_수열의_합.java
@@ -18,7 +18,37 @@ public class 연속된_부분_수열의_합 {
 
     static class Solution {
         public int[] solution(int[] sequence, int k) {
-            int[] answer = {};
+            int[] answer = new int[2];
+
+            int len = 10000001;
+            int left = 0;
+            int right = 0;
+            int sum = 0;
+            int flag = 0;
+
+            while (true) {
+
+                if (sum >= k) {
+
+                    sum -= sequence[left++];
+                    flag =0;
+
+                } else if (right >= sequence.length) {
+
+                    break;
+                } else if (sum < k) {
+
+                    sum += sequence[right++];
+                    flag = 1;
+
+                }
+
+                if (sum == k) {
+
+
+                }
+            }
+
 
 
             return answer;
