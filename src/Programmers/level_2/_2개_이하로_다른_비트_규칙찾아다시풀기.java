2cd3670a31f6f212b81710f03e1afe3f3792ad15
강희인 <dwoosung1@gmail.com>
2023-10-01 23:15:04 +0900

    알고리즘 문제풀이

--- src/Programmers/level_2/_2개_이하로_다른_비트_규칙찾아다시풀기.java
+++ src/Programmers/level_2/_2개_이하로_다른_비트_규칙찾아다시풀기.java
@@ -13,34 +13,68 @@ public class _2개_이하로_다른_비트_규칙찾아다시풀기 {
         System.out.println(Arrays.toString(solution.solution(numbers)));
 
     }
+//    static class Solution {
+//        public long[] solution(long[] numbers) {
+//            long[] answer = new long[numbers.length];
+//
+//            for (int i = 0; i < numbers.length; i++) {
+//
+//
+//                if (numbers[i] % 2 == 0) {
+//
+//                    answer[i] = numbers[i] + 1;
+//                    continue;
+//                }
+//
+//                String number = Long.toString(numbers[i], 2);
+//
+//                if (!number.contains("0")) {
+//
+//                    String temp = number.substring(0, 1) + "0" + number.substring(1);
+//                    answer[i] = Long.parseLong(temp, 2);
+//                } else {
+//
+//                    int idx = number.lastIndexOf("0");
+//
+//                    System.out.println(idx);
+//
+//                    number = number.substring(0, idx) + "10" + number.substring(idx + 2);
+//                    answer[i] = Long.parseLong(number, 2);
+//                }
+//            }
+//
+//
+//            return answer;
+//        }
+//    }
+
     static class Solution {
         public long[] solution(long[] numbers) {
             long[] answer = new long[numbers.length];
 
-            for (int i = 0; i < numbers.length; i++) {
+            for (int a = 0; a < numbers.length; a++) {
 
+                if (numbers[a] % 2 == 0) {
 
-                if (numbers[i] % 2 == 0) {
-
-                    answer[i] = numbers[i] + 1;
+                    answer[a] = numbers[a] + 1;
                     continue;
                 }
 
-                String number = Long.toString(numbers[i], 2);
+                String two_bit = Long.toBinaryString(numbers[a]);
 
-                if (!number.contains("0")) {
+                String word = "";
 
-                    String temp = number.substring(0, 1) + "0" + number.substring(1);
-                    answer[i] = Long.parseLong(temp, 2);
+                if (!two_bit.contains("0")) {
+
+                    word = two_bit.substring(0, 1) + "0" + two_bit.substring(1);
+                    answer[a] = Long.parseLong(word, 2);
                 } else {
 
-                    int idx = number.lastIndexOf("0");
-
-                    System.out.println(idx);
-
-                    number = number.substring(0, idx) + "10" + number.substring(idx + 2);
-                    answer[i] = Long.parseLong(number, 2);
+                    int last_idx = two_bit.lastIndexOf("0");
+                    word = two_bit.substring(0,last_idx) + "10" + two_bit.substring(last_idx + 2);
+                    answer[a] = Long.parseLong(word, 2);
                 }
+
             }
 
 
--- src/Programmers/level_2/k진수에서_소수_개수_구하기_다시풀기.java
+++ src/Programmers/level_2/k진수에서_소수_개수_구하기_다시풀기.java
@@ -1,6 +1,8 @@
 package Programmers.level_2;
 
 
+import java.util.Arrays;
+
 public class k진수에서_소수_개수_구하기_다시풀기 {
 
 
@@ -15,133 +17,82 @@ public class k진수에서_소수_개수_구하기_다시풀기 {
 
     }
 
-// 거대한 숫자에 감당이 안됨 소수 판별 다른버전을 써야함
+
 //    static class Solution {
+//
 //        public int solution(int n, int k) {
 //            int answer = 0;
+//            String num = Integer.toString(n, k);
+//            String[] num_array = num.split("0");
 //
-//            String s = Integer.toString(n, k);
-//
-//            int[] map = new int[1000000];
-//
-//            map[0] = 1;
-//            map[1] = 1;
-//
-//            // 소수 지도.
-//            for (int i = 2; i < map.length; i++) {
-//
-//                if (map[i] == 0) {
-//
-//                    for (int j = i + i; j < map.length; j = j + i) {
-//
-//                        map[j] = 1;
-//                    }
-//
+//            for(String s:num_array){
+//                if(s.equals("")) continue;
+//                long number = Long.parseLong(s);
+//                if(isPrime(number)){
+//                    answer++;
 //                }
 //            }
-//
-//            // 조건 4번
-//            if (s.length() == 1 && map[Integer.parseInt(s)] == 0) {
-//
-//                return 1;
-//            }
-//
-//            char[] words = s.toCharArray();
-//
-//            // 조건 2번
-//            int start = 0;
-//            String check = "";
-//
-//            while (words[start] != '0') {
-//
-//                check += words[start];
-//                start++;
-//            }
-//
-//            if (start > 0 && map[Integer.parseInt(check)] == 0) {
-//
-//                answer ++;
-//            }
-//
-//            // 조건 3번
-//            int last = words.length - 1;
-//            check = "";
-//
-//            while (words[last] != '0') {
-//
-//                last --;
-//            }
-//
-//            for (int i = last + 1; i < words.length; i++) {
-//
-//                check += words[i];
-//            }
-//
-//            if (last < words.length - 1 && map[Integer.parseInt(check)] == 0) {
-//
-//                answer ++;
-//            }
-//
-//            check = "";
-//            for (int i = start + 1; i < last; i++) {
-//
-//                if (words[i] != '0') {
-//
-//                    check += words[i];
-//
-//                } else {
-//
-//                    if (map[Integer.parseInt(check)] == 0) {
-//
-//                        // 조건 1번
-//                        if (words[i - check.length() - 1] == '0' ) {
-//
-//                            answer ++;
-//                            check = "";
-//
-//                        }
-//                    }
-//                }
-//
-//            }
-//
-//
 //            return answer;
 //        }
+//
+//        public static boolean isPrime(long num){
+//            if(num == 1){
+//                return false;
+//            }
+//
+//            if(num == 2) return true;
+//
+//            for(int i=2;i<=(int)Math.sqrt(num);i++){
+//                if(num % i == 0){
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//
+//
 //    }
 
     static class Solution {
-
         public int solution(int n, int k) {
             int answer = 0;
-            String num = Integer.toString(n, k);
-            String[] num_array = num.split("0");
 
-            for(String s:num_array){
-                if(s.equals("")) continue;
-                long number = Long.parseLong(s);
-                if(isPrime(number)){
-                    answer++;
+            String number_bit = Integer.toString(n, k);
+
+            String[] numbers_arr = number_bit.split("0");
+
+            for (int i = 0; i < numbers_arr.length; i++) {
+
+                if(check_prime(numbers_arr[i])) {
+
+                    answer ++;
                 }
             }
+
             return answer;
         }
 
-        public static boolean isPrime(long num){
-            if(num == 1){
+        private boolean check_prime(String s) {
+
+            int n = Integer.parseInt(s);
+
+            if (n == 1) {
+
                 return false;
+            } else if (n == 2) {
+
+                return true;
             }
+            for (int i = 2; i <= Math.sqrt(n); i++) {
 
-            if(num == 2) return true;
+                if (n % i == 0) {
 
-            for(int i=2;i<=(int)Math.sqrt(num);i++){
-                if(num % i == 0){
                     return false;
                 }
             }
+
             return true;
         }
-
-
     }
+
 }
