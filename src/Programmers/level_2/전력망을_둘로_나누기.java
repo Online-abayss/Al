708c034aee7d215ef04c73f717e054992a133415
강희인 <dwoosung1@gmail.com>
2023-10-16 20:47:17 +0900

    알고리즘 문제풀이

--- src/Programmers/level_2/전력망을_둘로_나누기.java
+++ src/Programmers/level_2/전력망을_둘로_나누기.java
@@ -2,6 +2,8 @@ package Programmers.level_2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class 전력망을_둘로_나누기 {
 
@@ -17,57 +19,119 @@ public class 전력망을_둘로_나누기 {
 
     }
 
-    static ArrayList<Integer>[] graph;
-    static int min;
+//    static ArrayList<Integer>[] graph;
+//    static int min;
+//    static class Solution {
+//        public int solution(int n, int[][] wires) {
+//
+//            graph = new ArrayList[n + 1];
+//            min = Integer.MAX_VALUE;
+//
+//            for (int i = 1; i <= n; i++) {
+//
+//                graph[i] = new ArrayList<>();
+//            }
+//
+//            for (int i = 0; i < wires.length; i++) {
+//
+//                int v1 = wires[i][0];
+//                int v2 = wires[i][1];
+//                graph[v1].add(v2);
+//                graph[v2].add(v1);
+//
+//                boolean[] visited = new boolean[n + 1];
+//
+//                graph[v1].remove(v2);
+//                graph[v2].remove(v1);
+//
+//                int cnt = dfs(1, visited);
+//
+//                int diff = Math.abs(cnt - (n - cnt));
+//                min = Math.min(min, diff);
+//
+//                graph[v1].add(v2);
+//                graph[v2].add(v1);
+//            }
+//
+//            return min;
+//        }
+//
+//        private int dfs(int v, boolean[] visited) {
+//
+//            visited[v] = true;
+//            int cnt = 1;
+//
+//            for (int next : graph[v]) {
+//
+//                if (!visited[next]) {
+//
+//                    cnt += dfs(next, visited);
+//                }
+//            }
+//
+//            return cnt;
+//
+//        }
+//    }
+
+
+    static int[][] graph;
     static class Solution {
         public int solution(int n, int[][] wires) {
+            int answer = n + 1;
 
-            graph = new ArrayList[n + 1];
-            min = Integer.MAX_VALUE;
+            graph = new int[n + 1][n + 1];
 
-            for (int i = 1; i <= n; i++) {
+            // 입력하고
+            for (int i = 0; i < wires.length; i++) {
 
-                graph[i] = new ArrayList<>();
+                graph[wires[i][0]][wires[i][1]] = 1;
+                graph[wires[i][1]][wires[i][0]] = 1;
             }
+
+            int a = 0;
+            int b = 0;
 
             for (int i = 0; i < wires.length; i++) {
 
-                int v1 = wires[i][0];
-                int v2 = wires[i][1];
-                graph[v1].add(v2);
-                graph[v2].add(v1);
+                a = graph[i][0];
+                b = graph[i][1];
 
-                boolean[] visited = new boolean[n + 1];
+                graph[a][b] = 0;
+                graph[b][a] = 0;
 
-                graph[v1].remove(v2);
-                graph[v2].remove(v1);
-
-                int cnt = dfs(1, visited);
-
-                int diff = Math.abs(cnt - (n - cnt));
-                min = Math.min(min, diff);
-
-                graph[v1].add(v2);
-                graph[v2].add(v1);
+                answer = Math.min(answer, bfs(n, a));
             }
 
-            return min;
+            return answer;
         }
 
-        private int dfs(int v, boolean[] visited) {
+        private int bfs(int n, int a) {
 
-            visited[v] = true;
+            boolean[] visited = new boolean[n + 1];
+
+            Queue<Integer> queue = new LinkedList<>();
             int cnt = 1;
 
-            for (int next : graph[v]) {
+            queue.add(a);
 
-                if (!visited[next]) {
+            while (!queue.isEmpty()) {
 
-                    cnt += dfs(next, visited);
+                visited[a] = true;
+                int point = queue.poll();
+
+                for (int i = 1; i <= n; i++) {
+
+                    if (!visited[i] && graph[point][i] == 1) {
+
+                        cnt ++;
+                        queue.add(i);
+                    }
                 }
+
             }
 
-            return cnt;
+            return Math.abs(n - 2 * cnt);
 
         }
     }
--- src/Programmers/level_2/호텔_대실.java
+++ src/Programmers/level_2/호텔_대실.java
@@ -17,76 +17,145 @@ public class 호텔_대실 {
 
     }
 
+//    static class Solution {
+//        public int solution(String[][] book_time) {
+//            int answer = 0;
+//
+//            // 시작시간 . diff_time을 하나에 넣어서 비교군으로 파악하기?
+//
+//            // 기록표가 있어야 비교 할 수 있을듯.
+//            int cnt = 0;
+//
+//            int[][] bookTime_int = new int[book_time.length][2];
+//
+//            for (int i = 0; i < book_time.length; i++) {
+//
+//                int start_time = Integer.parseInt(book_time[i][0].split(":")[0]) * 60 + Integer.parseInt(book_time[i][0].split(":")[1]);
+//                int end_time =  Integer.parseInt(book_time[i][1].split(":")[0]) * 60 + Integer.parseInt(book_time[i][1].split(":")[1]);
+//
+//                end_time += 10;
+//
+//                bookTime_int[i][0] = start_time;
+//                bookTime_int[i][1] = end_time;
+//
+//            }
+//
+//            Arrays.sort(bookTime_int, new Comparator<int[]>() {
+//                @Override
+//                public int compare(int[] o1, int[] o2) {
+//
+//                    if (o1[0] > o2[0]) {
+//
+//                        return 1;
+//                    } else if (o1[0] < o2[0]) {
+//
+//                        return -1;
+//                    } else {
+//
+//                        if (o1[1] > o2[1]) {
+//
+//                            return 1;
+//                        }
+//
+//                        else
+//                            return -1;
+//                    }
+//
+//                }
+//            });
+//
+//            PriorityQueue<Integer> rooms = new PriorityQueue<>();
+//
+//            for (int[] used_room : bookTime_int) {
+//
+//                if (rooms.size() == 0) {
+//
+//                    rooms.add(used_room[1]);
+//                    continue;
+//                }
+//
+//                int IsEmpty_room = rooms.peek();
+//
+//                if (used_room[0] >= IsEmpty_room) {
+//
+//                    rooms.poll();
+//                    rooms.add(used_room[1]);
+//                } else {
+//
+//                    rooms.add(used_room[1]);
+//                }
+//            }
+//
+//            return rooms.size();
+//        }
+//    }
+
     static class Solution {
         public int solution(String[][] book_time) {
-            int answer = 0;
+            int answer = 1;
 
-            // 시작시간 . diff_time을 하나에 넣어서 비교군으로 파악하기?
+            int[][] book_time_int = new int[book_time.length][book_time[0].length];
 
-            // 기록표가 있어야 비교 할 수 있을듯.
-            int cnt = 0;
+            for (int i = 0; i < book_time_int.length; i++) {
 
-            int[][] bookTime_int = new int[book_time.length][2];
+                String[] times = book_time[i];
 
-            for (int i = 0; i < book_time.length; i++) {
+                for (int j = 0; j < 2; j++) {
 
-                int start_time = Integer.parseInt(book_time[i][0].split(":")[0]) * 60 + Integer.parseInt(book_time[i][0].split(":")[1]);
-                int end_time =  Integer.parseInt(book_time[i][1].split(":")[0]) * 60 + Integer.parseInt(book_time[i][1].split(":")[1]);
+                    book_time_int[i][j] = Integer.parseInt(times[j].split(":")[0]) * 60 + Integer.parseInt(times[j].split(":")[1]);
 
-                end_time += 10;
+                    if (j == 1) {
 
-                bookTime_int[i][0] = start_time;
-                bookTime_int[i][1] = end_time;
-
+                        book_time_int[i][j] += 10;
+                    }
+                }
             }
 
-            Arrays.sort(bookTime_int, new Comparator<int[]>() {
+            Arrays.sort(book_time_int, new Comparator<int[]>() {
                 @Override
                 public int compare(int[] o1, int[] o2) {
 
-                    if (o1[0] > o2[0]) {
-
-                        return 1;
-                    } else if (o1[0] < o2[0]) {
+                    if (o1[0] < o2[0]) {
 
                         return -1;
+                    } else if (o1[0] > o2[0]) {
+
+                        return 1;
+
                     } else {
 
-                        if (o1[1] > o2[1]) {
+                        if (o1[1] < o2[1]) {
+
+                            return -1;
+                        } else {
 
                             return 1;
                         }
-
-                        else
-                            return -1;
                     }
+
 
                 }
             });
 
-            PriorityQueue<Integer> rooms = new PriorityQueue<>();
+            System.out.println(Arrays.deepToString(book_time_int));
 
-            for (int[] used_room : bookTime_int) {
+            for (int i = 1; i < book_time_int.length; i++) {
 
-                if (rooms.size() == 0) {
+                int pre_end_time = book_time_int[i - 1][1];
+                int now_start_time = book_time_int[i][0];
 
-                    rooms.add(used_room[1]);
-                    continue;
-                }
+                if (pre_end_time > now_start_time) {
 
-                int IsEmpty_room = rooms.peek();
 
-                if (used_room[0] >= IsEmpty_room) {
-
-                    rooms.poll();
-                    rooms.add(used_room[1]);
                 } else {
 
-                    rooms.add(used_room[1]);
+
                 }
             }
 
-            return rooms.size();
+
+
+            return answer;
         }
     }
 }
