0c9c32de14ceba2b1b67c1cecffe54e97763f06c
강희인 <dwoosung1@gmail.com>
2023-11-06 23:15:11 +0900

    알고리즘 문제풀이

--- src/Programmers/level_2/수식_최대화_다시풀기_생각비슷.java
+++ src/Programmers/level_2/수식_최대화_다시풀기_생각비슷.java
@@ -1,6 +1,7 @@
 package Programmers.level_2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class 수식_최대화_다시풀기_생각비슷 {
 
@@ -16,78 +17,139 @@ public class 수식_최대화_다시풀기_생각비슷 {
     }
 
 
-    static String[] three_simbol = {"*+-"};
+//    static String[] three_simbol = {"*+-"};
+//    static class Solution {
+//        public long solution(String expression) {
+//
+//            ArrayList<String> simbol = new ArrayList<>();
+//
+//            ArrayList<Long> numbers = new ArrayList<>();
+//
+//            String word = "";
+//            for (char ch : expression.toCharArray()) {
+//
+//                if (ch - '0' >= 0 ) {
+//
+//                    word += ch;
+//                } else {
+//
+//                    simbol.add(String.valueOf(ch));
+//                    numbers.add(Long.parseLong(word));
+//                    word = "";
+//                }
+//            }
+//            int last_simbol_idx = expression.lastIndexOf(simbol.get(simbol.size() - 1));
+//            numbers.add(Long.parseLong(expression.substring(last_simbol_idx + 1)));
+//
+//            System.out.println(numbers);
+//            System.out.println(simbol);
+//
+//            long max = Integer.MIN_VALUE;
+//
+//            for (int i = 0; i < three_simbol.length; i++) {
+//
+//                String[] rank_simbol = three_simbol[i].split("");
+//
+//                ArrayList<String> temp_simbol = new ArrayList<>(simbol);
+//                ArrayList<Long> temp_numbers = new ArrayList<>(numbers);
+//
+//                // + - *
+//                for (int j = 0; j < rank_simbol.length; j++) {
+//
+//
+//                    // 문제에 주어진 수식
+//
+//                    for (int k = 0; k < temp_simbol.size();) {
+//
+//                        if (!rank_simbol[j].equals(temp_simbol.get(k))) {
+//
+//                            System.out.println(k);
+//                            k++;
+//                            continue;
+//                        }
+//
+//                        if (temp_simbol.get(k).equals("+")) {
+//
+//                            temp_numbers.set(k, temp_numbers.get(k) + temp_numbers.get(k + 1));
+//                        } else if (temp_simbol.get(k).equals("*")) {
+//
+//                            temp_numbers.set(k, temp_numbers.get(k) * temp_numbers.get(k + 1));
+//                        } else if (temp_simbol.get(k).equals("-")) {
+//
+//                            temp_numbers.set(k, temp_numbers.get(k) - temp_numbers.get(k + 1));
+//                        }
+//
+//                        temp_numbers.remove(k + 1);
+//                        temp_simbol.remove(k);
+//
+//                    }
+//                }
+//
+//                max = Math.max(max, Math.abs(temp_numbers.get(0)));
+//            }
+//
+//            return max;
+//        }
+//    }
+
+    static String[] simbol_arr = {"*+-","*-+","+-*","+*-","-*+","-+*"};
     static class Solution {
         public long solution(String expression) {
-
-            ArrayList<String> simbol = new ArrayList<>();
+            long answer = 0;
 
             ArrayList<Long> numbers = new ArrayList<>();
+            ArrayList<String> simbol = new ArrayList<>();
 
             String word = "";
-            for (char ch : expression.toCharArray()) {
 
-                if (ch - '0' >= 0 ) {
+            String[] test = expression.split("[0-9]");
 
-                    word += ch;
-                } else {
+            for (String s : test) {
 
-                    simbol.add(String.valueOf(ch));
-                    numbers.add(Long.parseLong(word));
-                    word = "";
+                if (!s.equals("")) {
+
+                    simbol.add(s);
                 }
             }
-            int last_simbol_idx = expression.lastIndexOf(simbol.get(simbol.size() - 1));
-            numbers.add(Long.parseLong(expression.substring(last_simbol_idx + 1)));
+
+            test = expression.split("[*+-]");
+
+            for (String s : test) {
+
+                numbers.add(Long.parseLong(s));
+            }
 
             System.out.println(numbers);
             System.out.println(simbol);
 
-            long max = Integer.MIN_VALUE;
+            for (int i = 0 ;i < simbol_arr.length; i++) {
 
-            for (int i = 0; i < three_simbol.length; i++) {
+                String[] start_simbol = simbol_arr[i].split("");
 
-                String[] rank_simbol = three_simbol[i].split("");
-
+                ArrayList<Long> temp_number = new ArrayList<>(numbers);
                 ArrayList<String> temp_simbol = new ArrayList<>(simbol);
-                ArrayList<Long> temp_numbers = new ArrayList<>(numbers);
 
-                // + - *
-                for (int j = 0; j < rank_simbol.length; j++) {
+                for (int j = 0; j < start_simbol.length; j++) {
 
-
-                    // 문제에 주어진 수식
 
                     for (int k = 0; k < temp_simbol.size();) {
 
-                        if (!rank_simbol[j].equals(temp_simbol.get(k))) {
+                        if (!start_simbol[j].equals(temp_simbol.get(k))) {
 
-                            System.out.println(k);
                             k++;
                             continue;
                         }
 
-                        if (temp_simbol.get(k).equals("+")) {
+                        if (start_simbol[j].equals("+")) {
 
-                            temp_numbers.set(k, temp_numbers.get(k) + temp_numbers.get(k + 1));
-                        } else if (temp_simbol.get(k).equals("*")) {
 
-                            temp_numbers.set(k, temp_numbers.get(k) * temp_numbers.get(k + 1));
-                        } else if (temp_simbol.get(k).equals("-")) {
-
-                            temp_numbers.set(k, temp_numbers.get(k) - temp_numbers.get(k + 1));
                         }
-
-                        temp_numbers.remove(k + 1);
-                        temp_simbol.remove(k);
-
                     }
                 }
-
-                max = Math.max(max, Math.abs(temp_numbers.get(0)));
             }
 
-            return max;
+
+            return answer;
         }
     }
 }
--- src/Programmers/level_2/숫자_카드_나누기_다시풀기.java
+++ src/Programmers/level_2/숫자_카드_나누기_다시풀기.java
@@ -5,14 +5,73 @@ public class 숫자_카드_나누기_다시풀기 {
     public static void main(String[] args) {
 
 
-        int[] arrayA = {10, 17};
-        int[] arrayb = {5, 20};
+        int[] arrayA = {10, 20};
+        int[] arrayb = {5, 17};
 
         Solution solution = new Solution();
 
         System.out.println(solution.solution(arrayA, arrayb));
 
     }
+
+//    static class Solution {
+//        public int solution(int[] arrayA, int[] arrayB) {
+//            int answer = 0;
+//
+//            int gcd_a = arrayA[0];
+//            int gcd_b = arrayB[0];
+//
+//            for (int i = 1; i < arrayA.length; i++) {
+//
+//                gcd_a = gcd(gcd_a, arrayA[i]);
+//                gcd_b = gcd(gcd_b, arrayB[i]);
+//            }
+//
+//            if (notdisivisble(arrayB, gcd_a)) {
+//
+//                if (answer < gcd_a) {
+//
+//                    answer = gcd_a;
+//                }
+//            }
+//
+//            if (notdisivisble(arrayA, gcd_b)) {
+//
+//                if (answer < gcd_b) {
+//
+//                    answer = gcd_b;
+//                }
+//            }
+//
+//
+//            return answer;
+//
+//
+//        }
+//
+//        private boolean notdisivisble(int[] arrayA, int gcd_b) {
+//
+//            for (int n : arrayA) {
+//
+//                if (n % gcd_b == 0) {
+//
+//                    return false;
+//                }
+//            }
+//
+//            return true;
+//        }
+//
+//        private int gcd(int a, int b) {
+//
+//            if (a % b == 0) {
+//
+//                return b;
+//            }
+//            return gcd(b, a % b);
+//        }
+//    }
+
 
     static class Solution {
         public int solution(int[] arrayA, int[] arrayB) {
@@ -21,54 +80,51 @@ public class 숫자_카드_나누기_다시풀기 {
             int gcd_a = arrayA[0];
             int gcd_b = arrayB[0];
 
-            for (int i = 1; i < arrayA.length; i++) {
+            for (int a = 1; a < arrayA.length; a++) {
 
-                gcd_a = gcd(gcd_a, arrayA[i]);
-                gcd_b = gcd(gcd_b, arrayB[i]);
+                gcd_a = gcd(arrayA[a], gcd_a);
             }
 
-            if (notdisivisble(arrayB, gcd_a)) {
+            for (int a = 1; a < arrayB.length; a++) {
 
-                if (answer < gcd_a) {
-
-                    answer = gcd_a;
-                }
+                gcd_b = gcd(arrayB[a], gcd_b);
             }
 
-            if (notdisivisble(arrayA, gcd_b)) {
+            int a = 0;
+            int b = 0;
 
-                if (answer < gcd_b) {
 
-                    answer = gcd_b;
-                }
-            }
+            a = able_divided_number(arrayB, gcd_a);
 
+            b = able_divided_number(arrayA, gcd_b);
+
+            answer = Math.max(a, b);
 
             return answer;
-
-
         }
 
-        private boolean notdisivisble(int[] arrayA, int gcd_b) {
+        private int able_divided_number(int[] b, int gcd_a) {
 
-            for (int n : arrayA) {
+            for (int i = 0; i < b.length; i++) {
 
-                if (n % gcd_b == 0) {
+                if (b[i] % gcd_a == 0) {
 
-                    return false;
+                    return 0;
                 }
+
             }
 
-            return true;
+            return gcd_a;
         }
 
-        private int gcd(int a, int b) {
+        private int gcd(int i, int gcd_a) {
 
-            if (a % b == 0) {
+            if (i % gcd_a == 0) {
 
-                return b;
+                return  gcd_a;
             }
-            return gcd(b, a % b);
+
+            return gcd(gcd_a, i % gcd_a);
         }
     }
 }
